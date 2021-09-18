9c124a700f2530a1d55646d8c3fbb997b0ac4e69
Ahmed Arnaout <ahmed.arnaout123@gmail.com>
2021-09-18 10:43:35 +0300

    Added <Category> Property to <Product> Entity

--- src/Services/Catalog/Catalog.API/Entities/Product.cs
+++ src/Services/Catalog/Catalog.API/Entities/Product.cs
@@ -16,6 +16,8 @@ namespace Catalog.API.Entities
         [BsonElement("Name")]
         public string Name { get; set; }
 
+        public string Category { get; set; }
+
         public string Description { get; set; }
 
         public string Summary { get; set; }
