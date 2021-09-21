3ff015a71420c373228bb287e8e09df26e0ba1ce
Ahmed Arnaout <ahmed.arnaout123@gmail.com>
2021-09-21 10:44:02 +0300

    Added <Coupon> Entity

---
+++ src/Services/Discount/Discount.API/Entities/Coupon.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Discount.API.Entities
+{
+    public class Coupon
+    {
+        public string Id { get; set; }
+
+        public int ProductName { get; set; }
+
+        public string Description { get; set; }
+
+        public int Amount { get; set; }
+    }
+}
