01aec12c4cbe8a4c73d0f7fc2a93bf31feb2efa6
Ahmed Arnaout <ahmed.arnaout123@gmail.com>
2021-10-11 19:18:11 +0300

    Created "DiscountGrpcService" Class.
    This class provides an abstraction on accessing the discount grpc client, so we can invoke the methods provided in this class from anywhere without knowing how communication between the grpc client and server is done.
    In my personal opinion, the idea of this class is smart but it should've been implemented in this way, first we create an interface with the name "IDiscountGrpcClientService" which defines the methods that a grpc client would need, then we implement this interface by this class where the name can be set to match the interface name.
    In order to keep consistency with the course, the implementation was left as it was introduced in the course.

---
+++ src/Services/Basket/Basket.API/GrpcServices/DiscountGrpcService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Discount.Grpc.Protos;
+
+namespace Basket.API.GrpcServices
+{
+    public class DiscountGrpcService
+    {
+        private readonly DiscountService.DiscountServiceClient _discountGrpcClient;
+
+        public DiscountGrpcService(DiscountService.DiscountServiceClient discountGrpcClient)
+        {
+            _discountGrpcClient = discountGrpcClient;
+        }
+
+        public async Task<CouponModel> GetDiscount(string productName)
+        {
+            var discountRequest = new GetDiscountRequest { ProductName = productName };
+
+            return await _discountGrpcClient.GetDiscountAsync(discountRequest);
+        }
+
+
+    }
+}
