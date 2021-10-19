26b407908461466634b68d4fcd9b5b62445a1598
Ahmed Arnaout <ahmed.arnaout123@gmail.com>
2021-10-19 08:31:55 +0300

    Added Persistence To The Infrastructure Layer.
    - Used entity framework core with Sql Server dependency.
    - Created "OrderContext" class that represents a db context, and added a "DbSet<Order>" that represents orders table.
    - Created "OrderContextSeed" to seed orders to the database if there are no entries.

---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContextSeed.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Ordering.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    public class OrderContextSeed
+    {
+        public static async void SeedOrders(OrderContext ordersContext, ILogger<OrderContextSeed> logger)
+        {
+            if(ordersContext.Orders.Any())
+            {
+                ordersContext.Orders.AddRange(GetPreconfiguredOrders());
+                await ordersContext.SaveChangesAsync();
+                logger.LogInformation("Orders Database was seeded with context {OrderContext}", typeof(OrderContext).Name);
+            }
+        }
+
+        private static IEnumerable<Order> GetPreconfiguredOrders()
+        {
+            return new List<Order>
+            {
+                new Order() {UserName = "swn", FirstName = "Mehmet", LastName = "Ozkaya", EmailAddress = "ezozkme@gmail.com", AddressLine = "Bahcelievler", Country = "Turkey", TotalPrice = 350 }
+            };
+        }
+    }
+}
