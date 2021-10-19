fc1e8f469fa001b968d827ff58c533fe3fc0c2f6
Ahmed Arnaout <ahmed.arnaout123@gmail.com>
2021-10-19 11:15:29 +0300

    Added The Infrastructure Layer Dpendency Injection.

---
+++ src/Services/Ordering/Ordering.Infrastructure/InfrastructureServiceRegisteration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Contracs.infrastructure;
+using Ordering.Application.Contracs.Persistence;
+using Ordering.Application.Models;
+using Ordering.Infrastructure.Persistence;
+using Ordering.Infrastructure.Repositories;
+using Ordering.Infrastructure.Mail;
+
+namespace Ordering.Infrastructure
+{
+    public class InfrastructureServiceRegisteration
+    {
+        public static void AddInfrastructureService(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<OrderContext>(options => options.UseSqlServer(configuration.GetConnectionString("OrderingConnectionString")));
+
+            services.AddScoped(typeof(IAsyncRepository<>), typeof(RepositoryBase<>));
+            services.AddScoped<IOrderRepository, OrderRepository>();
+
+            services.Configure<EmailSettings>(c => configuration.GetSection("EmailSettings"));
+            services.AddTransient<IEmailService, EmailService>();
+        }
+    }
+}
