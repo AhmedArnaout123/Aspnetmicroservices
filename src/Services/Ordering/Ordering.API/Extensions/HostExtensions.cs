f91bd42a0d173ee59594d0164b2b29da71813c16
Ahmed Arnaout <ahmed.arnaout123@gmail.com>
2021-10-19 13:34:03 +0300

    Create Migrate Database Extension.

---
+++ src/Services/Ordering/Ordering.API/Extensions/HostExtensions.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ordering.API.Extensions
+{
+    public static class HostExtensions
+    {
+        public static IHost MigrateDatabase<TContext>(this IHost host, 
+                                                      Action<TContext, IServiceProvider> seeder, 
+                                                      int? retry = 0) where TContext : DbContext
+        {
+            int retryForAvailability = retry.Value;
+
+            using(var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var logger = services.GetRequiredService<ILogger<TContext>>();
+                var context = services.GetService<TContext>();
+
+                try
+                {
+                    logger.LogInformation("Migrated database associated with context {TContext} started", typeof(TContext).Name);
+
+                    InvokeSeeder(seeder, context, services);
+
+                    logger.LogInformation("Migrated database associated with context {TContext} finished successfully", typeof(TContext).Name);
+
+                }
+                catch(SqlException ex)
+                {
+                    logger.LogError(ex, "An error ocurred while migrating the database usded on context {context}", typeof(TContext).Name);
+
+                    if(retryForAvailability < 50)
+                    {
+                        retryForAvailability++;
+                        System.Threading.Thread.Sleep(2000);
+                        MigrateDatabase<TContext>(host, seeder, retryForAvailability);
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return host;
+            }
+        }
+
+        private static void InvokeSeeder<TContext>(Action<TContext, IServiceProvider> seeder, TContext context, IServiceProvider services) where TContext : DbContext
+        {
+            context.Database.Migrate();
+            seeder(context, services);
+        }
+    }
+}
